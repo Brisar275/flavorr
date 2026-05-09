4a4b3c8727beb6b8ce3c7e5e195a3191f250d07e
BrisaAimee <brisaaimeerodriguez@gmail.com>
2026-05-09 03:46:51 -0300

    detalles y tipografias

--- src/app/componentes/Navbar.js
+++ src/app/componentes/Navbar.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-900 p-4 flex items-center justify-between text-white">
-      <h1>Flavor</h1>
+    <nav className="bg-blue-900 py-4 px-13 flex items-center justify-between text-white">
+      <h1 className="font-[Aurora] text-2xl">Flavor</h1>
       <div>
-        <ul className="flex flex-nowrap gap-4  text-white">
+        <ul className="flex flex-nowrap gap-4 font-[Inter] text-white">
           <li><a href="/">Inicio</a></li>
           <li><a href="/about">Sobre Nosotros</a></li>
           <li><a href="/contact">Contacto</a></li>
