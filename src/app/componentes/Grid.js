ab4d2b01a046bea4e11d922abeb3c640865f99cd
BrisaAimee <brisaaimeerodriguez@gmail.com>
2026-05-14 16:45:10 -0300

    arreglo tema oscuro que no mostraba el titulo

--- src/app/componentes/Grid.js
+++ src/app/componentes/Grid.js
@@ -32,14 +32,14 @@ const Grid = () => {
   }, []);
 
   return (
-    <div>
-      <h2 className="text-blue-900 font-[Aurora] text-4xl px-15 pt-15">
+    <div className="bg-white px-15 py-20">
+      <h2 className="text-blue-900 font-[Aurora] text-4xl p-5">
         Recetas
       </h2>
 
       <div
         id="recetas"
-        className="bg-white px-15 py-20 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 p-4 items-stretch"
+        className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 p-4 items-stretch"
       >
         {!loading &&
           data.map((recipe) => (
